672e8c4ced30c5ceb1d8d39352cc9abcf40e570f
NasirAhmad <G00419630@atu.ie>
2025-11-16 14:48:43 +0000

    added code for connection to mongodb

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, HTTPException
+from configurations import collection
+from database.schemas import all_tasks
+from database.models import Todo
 
 app = FastAPI()
+router = APIRouter()
 
-@app.get("/")
-async def homepage():
-    return{"message":"hello world"}
+
+
+@router.get("/get")
+async def get_all_todos():
+    data = collection.find()
+    return all_tasks(data)
+
+@router.post("/post")
+async def create_task(new_task:Todo):
+    try:
+        response = collection.insert_one(dict(new_task))
+        return {"status code":200,"id":str(response.inserted_id)}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"some error occured {e}")
+
+app.include_router(router)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
